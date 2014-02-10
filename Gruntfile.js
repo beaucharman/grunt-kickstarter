84565151fa2a1ef36dd7a183ef22affc72b73cc5
Beau Charman <beau.charman@gmail.com>
2014-02-10 23:18:24 +1100

    line up comments

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,43 +1,41 @@
 /**
-*
-* Gruntfile
-* ================================================
-* Grunt installation:
-*
-*		npm install -g grunt-cli
-*		npm install -g grunt-init
-*
-* Project Dependencies:
-*
-*		npm install grunt --save-dev
-*   npm install grunt-autoprefixer --save-dev
-*   npm install grunt-contrib-csslint --save-dev
-*   npm install grunt-contrib-cssmin --save-dev
-*   npm install grunt-contrib-imagemin --save-dev
-*   npm install grunt-contrib-jshint --save-dev
-*   npm install grunt-contrib-sass --save-dev
-*   npm install grunt-contrib-uglify --save-dev
-*   npm install grunt-contrib-watch --save-dev
-*   npm install jshint-stylish --save-dev
-*   npm install load-grunt-tasks --save-dev
-*   npm install time-grunt --save-dev
-*
-* npm install grunt-contrib-csslint --save-dev
-*
-* Simple Dependency Install (with a package.josn):
-*
-*		npm install
-*
-* Install with no package.json
-*
-*		npm init
-*   npm install grunt grunt-autoprefixer grunt-contrib-csslint grunt-contrib-cssmin grunt-contrib-imagemin grunt-contrib-jshint grunt-contrib-sass grunt-contrib-uglify grunt-contrib-watch jshint-stylish load-grunt-tasks time-grunt --save-dev
-*
-* Gem Dependencies:
-*
-*		gem install image_optim
-*
-*/
+ *
+ * Gruntfile
+ * ================================================
+ * Grunt installation:
+ *
+ *   npm install -g grunt-cli
+ *   npm install -g grunt-init
+ *
+ * Project Dependencies:
+ *
+ *   npm install grunt --save-dev
+ *   npm install grunt-autoprefixer --save-dev
+ *   npm install grunt-contrib-csslint --save-dev
+ *   npm install grunt-contrib-cssmin --save-dev
+ *   npm install grunt-contrib-imagemin --save-dev
+ *   npm install grunt-contrib-jshint --save-dev
+ *   npm install grunt-contrib-sass --save-dev
+ *   npm install grunt-contrib-uglify --save-dev
+ *   npm install grunt-contrib-watch --save-dev
+ *   npm install jshint-stylish --save-dev
+ *   npm install load-grunt-tasks --save-dev
+ *   npm install time-grunt --save-dev
+ *
+ * Simple Dependency Install (with a package.josn):
+ *
+ *   npm install
+ *
+ * Install with no package.json
+ *
+ *   npm init
+ *   npm install grunt grunt-autoprefixer grunt-contrib-csslint grunt-contrib-cssmin grunt-contrib-imagemin grunt-contrib-jshint grunt-contrib-sass grunt-contrib-uglify grunt-contrib-watch jshint-stylish load-grunt-tasks time-grunt --save-dev
+ *
+ * Gem Dependencies:
+ *
+ *   gem install image_optim
+ *
+ */
 
 module.exports = function (grunt) {
 
